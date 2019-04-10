252ce8e512d17ec199f407d0ac6622f47f1f6d16
vuyisile <vuyisilemark@gmail.com>
2019-04-10 12:42:46 +0200

    modification to ATM.js

--- ATM.js
+++ ATM.js
@@ -1,21 +1,28 @@
-var availableAmount = 500;
-console.log("availableAmount", availableAmount);
-var withdrawalAmount = 400;
-console.log("withdrawalAmount", withdrawalAmount)
-var balance = availableAmount - withdrawalAmount
+var availableBalance = 500;
+var transactionHistory = [];
 
-function withdraw(availableAmount) {
-    
-    if (availableAmount == withdrawalAmount) {
-        //  balance = subract(availableAmount, withdrawalAmount);
-        return "withdrawn";
-    } else if (availableAmount < withdrawalAmount){
-        return "insufficient amount";
-     }else{
-         console.log("insufficient fund"); 
-     }
+function giveUserBalance() {
+    return availableBalance;
+}
+
+function withdraw(withdrawalAmount) {
+
+    if (availableBalance >= withdrawalAmount) {
+        availableBalance = availableBalance - withdrawalAmount;
+        transactionHistory.push(withdrawalAmount);
+        console.log("transactionHistory", transactionHistory)
+        console.log("withrawal of " + withdrawalAmount + " withdrawn")
+        console.log("remaining funds " + availableBalance)
+        return
+    }
+    else if(availableBalance < withdrawalAmount){
+        console.log("Insufficient Balance")
+        return "Insufficient Balance";
+    }
 
 }
-// console.log(withdraw(4000));
-var total= balance;
-console.log("current balance", total);
+
+console.log("current balance " + giveUserBalance())
+withdraw(200);
+withdraw(310);
+
