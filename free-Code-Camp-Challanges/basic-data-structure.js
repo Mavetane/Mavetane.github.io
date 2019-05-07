0044e2f2c7d7b9617b473941aba836d404c63124
vuyisile <vuyisilemark@gmail.com>
2019-05-07 10:59:39 +0200

    modification to topic

--- free-Code-Camp-Challanges/basic-data-structure.js
+++
@@ -1,9 +0,0 @@
-function mixedNumbers(arr) {
-    // change code below this line
-  
-    // change code above this line
-    return arr;
-  }
-  
-  // do not change code below this line
-  console.log(mixedNumbers(['IV', 5, 'six']));
