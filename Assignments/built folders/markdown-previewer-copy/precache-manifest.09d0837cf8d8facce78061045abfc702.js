48afca7318b6bafa2e122351d223cede272951fa
Mavetane <collenmaphike25@gmail.com>
2019-09-13 09:34:06 +0200

    modification

---
+++ Assignments/built folders/markdown-previewer-copy/precache-manifest.09d0837cf8d8facce78061045abfc702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c71c8353b26c1bc1e04186347370641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bd7700bc51d06cc4f96",
+    "url": "/static/css/main.ce0dec93.chunk.css"
+  },
+  {
+    "revision": "11e7c41093670cbd7384",
+    "url": "/static/js/2.088efcd6.chunk.js"
+  },
+  {
+    "revision": "1bd7700bc51d06cc4f96",
+    "url": "/static/js/main.91842d55.chunk.js"
+  },
+  {
+    "revision": "9708fe94a06370cae267",
+    "url": "/static/js/runtime~main.a4964ae6.js"
+  }
+]);
