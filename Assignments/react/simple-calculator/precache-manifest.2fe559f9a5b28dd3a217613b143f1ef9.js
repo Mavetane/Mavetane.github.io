ceb74a9f085b8bb6e34699c2170c89b673107b6e
Mavetane <collenmaphike25@gmail.com>
2019-11-06 11:05:52 +0200

    deployed

---
+++ Assignments/react/simple-calculator/precache-manifest.2fe559f9a5b28dd3a217613b143f1ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa062fb0fff39d535846b32efa5d015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f07b57f45306c162e51",
+    "url": "/static/css/main.f0483c38.chunk.css"
+  },
+  {
+    "revision": "49c5fbc19053470ce7b4",
+    "url": "/static/js/2.e8b3ab44.chunk.js"
+  },
+  {
+    "revision": "0f07b57f45306c162e51",
+    "url": "/static/js/main.00062b62.chunk.js"
+  },
+  {
+    "revision": "b67f7098fb4c64bd0827",
+    "url": "/static/js/runtime-main.7b47036a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
