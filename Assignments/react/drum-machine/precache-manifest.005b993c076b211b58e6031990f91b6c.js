13051f928c9dd3971062a06bb5b8d64385da34d8
Mavetane <collenmaphike25@gmail.com>
2019-11-06 10:45:24 +0200

    Project complete

---
+++ Assignments/react/drum-machine/precache-manifest.005b993c076b211b58e6031990f91b6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0ddc1361136aad52a35402ab3bee40",
+    "url": "/Mavetane.github.io/Assignments/react/drum-machine/index.html"
+  },
+  {
+    "revision": "03f6df52380fdd1e7df5",
+    "url": "/Mavetane.github.io/Assignments/react/drum-machine/static/css/main.5e00f448.chunk.css"
+  },
+  {
+    "revision": "82006b08e9fa60638fc5",
+    "url": "/Mavetane.github.io/Assignments/react/drum-machine/static/js/2.25be781b.chunk.js"
+  },
+  {
+    "revision": "03f6df52380fdd1e7df5",
+    "url": "/Mavetane.github.io/Assignments/react/drum-machine/static/js/main.a4b597d2.chunk.js"
+  },
+  {
+    "revision": "b73cb60c9d9c2a9674d1",
+    "url": "/Mavetane.github.io/Assignments/react/drum-machine/static/js/runtime-main.781bc915.js"
+  }
+]);
