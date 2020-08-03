5826aff76f96bd6127052b6efeecff4adfc0255a
Owethu99Twala <owethutwala99@gmail.com>
2020-08-03 09:11:18 +0200

    fixing

---
+++ reccursions/precache-manifest.1adb6452dfeb5599a6998bb1a3f51d1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2618311096670d7cd10167594507069",
+    "url": "/reccursion/index.html"
+  },
+  {
+    "revision": "6f0409245818cdaf7882",
+    "url": "/reccursion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a10e923dfd8949c24fb7",
+    "url": "/reccursion/static/js/2.1449581f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reccursion/static/js/2.1449581f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0409245818cdaf7882",
+    "url": "/reccursion/static/js/main.3fe35235.chunk.js"
+  },
+  {
+    "revision": "52134ae665a2bdd80768",
+    "url": "/reccursion/static/js/runtime-main.e7b6453e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reccursion/static/media/logo.5d5d9eef.svg"
+  }
+]);
