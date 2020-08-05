8e0b7a0c7397483975b1b69a7c147fa4e34ff2f3
Owethu99Twala <owethutwala99@gmail.com>
2020-08-05 14:52:41 +0200

    c sharp automation

---
+++ chromeopener/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace chromeopener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://google.com");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
